de4f57633674062dc47cbbcde296cf74f0dcf421
melber <melber369@gmail.com>
2018-10-12 12:00:47 +1100

    question 3 answered

--- christoph/answers.rb
+++ christoph/answers.rb
@@ -5,7 +5,7 @@ def offer_rose(person)
   puts "Would you take this rose, #{person}, in exchange for giving an old beggar woman shelter from bitter cold?"
 end
 
-#Question 2
+# Question 2
 town = {
   residents: ["Maurice", "Belle", "Gaston"],
   castle: {
@@ -16,3 +16,10 @@ town = {
 }
 town[:residents].delete("Belle")
 town[:castle][:guests].push("Belle")
+
+# Question 3
+friends = ["Chip Potts", "Cogsworth", "Lumière", "Mrs. Potts"]
+
+friends.each do |name|
+  puts "Belle is friends with #{name}"
+end
