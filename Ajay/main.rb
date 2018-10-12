65ba8cbc6bc2811043e95f2ef7fafbf724514c39
AjayRaveendran <ajay.raveendran@gmail.com>
2018-10-12 12:00:01 +1100

    question 2

--- Ajay/main.rb
+++ Ajay/main.rb
@@ -1,6 +1,20 @@
 # Question 1
-def offer_rose(person)
-  puts "Would you take this rose, #{person}, in exchange for giving an old beggar woman shelter from the bitter cold?"
-end
+# def offer_rose(person)
+#   puts "Would you take this rose, #{person}, in exchange for giving an old beggar woman shelter from the bitter cold?"
+# end
 
-offer_rose('Ajay')
+# offer_rose('Ajay')
+
+#Question 2
+town = {
+  residents: ["Maurice", "Belle", "Gaston"],
+  castle: {
+    num_rooms: 47,
+    residents: ["Robby Benson"],
+    guests: []
+  }
+}
+
+town[:residents].delete('Belle')
+
+
