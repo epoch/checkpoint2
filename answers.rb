407b646e99bee3d60efa3f75448359ac847a22bd
Karen Kuek <karenkk75@gmail.com>
2018-10-12 12:37:25 +1100

    qn 5 done

--- answers.rb
+++ answers.rb
@@ -77,11 +77,13 @@ end
 # # Question 5
 # Assume the following array:
 # ```ruby
-# children = ['Wendy', 'John', 'Michael']
+children = ['Wendy', 'John', 'Michael']
 # ```
 # Use `.map` to iterate through the `children` array and add ` Darling` to the end
 # of their names. Assign the returned array to a variable called `darling_children`.
 # Example: `Wendy` should become `Wendy Darling` in the new array.
+
+children.map { |name| name + " Darling" }
 
 
 # # Question 6
