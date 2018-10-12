ea3275cf5ba3bd9f59741190ac839a0265812cb2
Andrew Godman <andrew.godman@me.com>
2018-10-12 11:59:05 +1100

    added question 2 answers

--- andrew.rb
+++ andrew.rb
@@ -6,4 +6,18 @@ def offer_rose(person)
     "Would you take this rose, #{person} in exchange for giving an old beggar woman shelter from the bitter cold?"
 end
 
+# Question 2
+
+town = {
+  residents: ["Maurice", "Belle", "Gaston"],
+  castle: {
+    num_rooms: 47,
+    residents: ["Robby Benson"],
+    guests: []
+  }
+}
+
+town[:residents].delete("Belle")
+town[:castle][:residents].push("Belle")
+
 binding.pry
