2ca0d9ca7994dbc26db248a6d69774049c8bff9d
Andrew Godman <andrew.godman@me.com>
2018-10-12 11:51:28 +1100

    added Question 1 answer

--- andrew.rb
+++ andrew.rb
@@ -1,3 +1,9 @@
 require 'pry'
 
+# Question 1
+
+def offer_rose(person)
+    "Would you take this rose, #{person} in exchange for giving an old beggar woman shelter from the bitter cold?"
+end
+
 binding.pry
