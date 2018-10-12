4f59a52f2e587e40a6e612def76ca03fd7dd4786
jamesrbedwell <jamesrbedwell@gmail.com>
2018-10-12 11:47:09 +1100

    Question 2

--- james/checkpoint2.rb
+++ james/checkpoint2.rb
@@ -3,4 +3,14 @@ def offer_rose(person)
  p "Would you take this rose, #{person}, in exchange for giving an old beggar woman shelter from the bitter cold?"
 end
 
-offer_rose('James')
+#Question 2
+town = {
+  residents: ["Maurice", "Belle", "Gaston"],
+  castle: {
+    num_rooms: 47,
+    residents: ["Robby Benson"],
+    guests: []
+  }
+}
+
+town[:residents].delete("Belle")
